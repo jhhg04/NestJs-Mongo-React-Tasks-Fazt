e8281e8262dcba3870e41f7499d67bc656378310
jhhg04 <jhhg04@gmail.com>
2023-12-29 18:08:48 -0500

    add front

--- tasksfront/src/App.tsx
+++ tasksfront/src/App.tsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import TaskForm from './components/TaskForm';
+import TasksList from './components/TasksList';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='h-screen bg-zinc-900 text-white flex justify-center items-center'>
+      <div className='bg-gray-950 p-4 w-2/5'>
+        <h1 className='text-3xl font-bold text-center block my-2'>Task App</h1>
+        <TaskForm />
+        <TasksList />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
